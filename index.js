87a5ad7f363c2d2cf976e0a072eb5e6b88b25728
256hz <a.d@256hz.com>
2019-04-30 20:29:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+const testVar = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "X"},
+  {year: "2016", result: "N/A"}
+]
+
+function testFunc(data) {
+  let winner = data.find( obj => obj.result === "W" )
+  if (winner) {
+    return winner.year
+  } else {
+    return undefined
+  }
 }
+
+console.log(testFunc(testVar))
