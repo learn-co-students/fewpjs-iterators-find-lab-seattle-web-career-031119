1d368e450d44421d93a67b59714a115bd2f5b4c2
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-05-01 11:33:52 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  const x = array.find( (obj) => obj['result'] === "W")
+  console.log(x)
+  if (x) {
+    return x['year']
+  } else {
+    return x
+  }
+}
